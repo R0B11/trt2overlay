bdf083278cb23a5f03b437e99abc010056df722f
illonion <zhou.lawrence.yz@gmail.com>
2023-07-03 11:47:56 +1000

    Clear all player tiles

--- brackets/index.js
+++ brackets/index.js
@@ -35,3 +35,34 @@ const toLoserBracket = () => {
     lowerBracket.css("opacity", 1)
 }
 const pullResultsFromDatabase = () => {}
+
+// Reset Bracket
+let winnerBracketRound1 = document.getElementsByClassName("winnerBracketRound1")
+let winnerBracketRound2 = document.getElementsByClassName("winnerBracketRound2")
+let winnerBracketRound3 = document.getElementsByClassName("winnerBracketRound3")
+let winnerBracketRound4 = document.getElementsByClassName("winnerBracketRound4")
+let winnerBracketRound5And6Container = document.getElementsByClassName("winnerBracketRound5And6Container")
+let lowerBracketPlayer = document.getElementsByClassName("lowerBracketPlayer")
+let lowerBracketFinalsPlayer = document.getElementsByClassName("lowerBracketFinalsPlayer")
+
+let playerTiles = [winnerBracketRound1, winnerBracketRound2, winnerBracketRound3, winnerBracketRound4, winnerBracketRound5And6Container, lowerBracketPlayer, lowerBracketFinalsPlayer]
+
+const resetBracket = function() {
+    // Clear Each Tile
+    for (let i = 0; i < playerTiles.length; i++) {
+        for (let j = 0; j < playerTiles[i].length; j++) {
+            playerTiles[i][j].style.backgroundColor = "var(--borderGray)"
+            playerTiles[i][j].children[1].style.backgroundColor = "var(--elementGray)"
+            playerTiles[i][j].children[1].style.color = "white"
+            playerTiles[i][j].children[1].innerText = ""
+            playerTiles[i][j].children[2].style.backgroundColor = "var(--elementGray)"
+            playerTiles[i][j].children[2].style.backgroundImage = "none"
+            playerTiles[i][j].children[2].innerHTML = ""
+            playerTiles[i][j].children[3].style.color = "var(--textGray)"
+            playerTiles[i][j].children[3].innerText = "PLAYER"
+            playerTiles[i][j].children[4].style.backgroundColor = "var(--elementGray)"
+            playerTiles[i][j].children[4].style.backgroundImage = "none"
+            playerTiles[i][j].children[4].innerHTML = ""
+        }
+    }
+}
